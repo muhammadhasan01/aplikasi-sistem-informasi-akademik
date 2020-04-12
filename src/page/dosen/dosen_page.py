4ba49c5ba0754848595e7144401e88c421963da3
naufal-dean <naufal.dean@gmail.com>
2020-04-12 15:48:27 +0700

    Init dosen tab

--- src/page/dosen/dosen_page.py
+++ src/page/dosen/dosen_page.py
@@ -1,5 +1,5 @@
 from PyQt5.QtCore import QFile
-from PyQt5.QtWidgets import QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QPushButton
 from PyQt5 import uic
 
 import resource
@@ -16,14 +16,22 @@ def initDosenPage(window, auth):
     # Get object from layout ui
     _mainVLayout_D_1 = window.findChild(QVBoxLayout, "mainVLayout_D_1")
 
-    # Crate and load navbar widget
+    # Create and load navbar widget
     _navbar_D_1 = QWidget()
     uifile = QFile(":ui/ui/dosen_navbar.ui")
     uifile.open(QFile.ReadOnly)
     uic.loadUi(uifile, _navbar_D_1)
     uifile.close()
+    # Get object from navbar ui
+    _berandaButton_D_2 = _navbar_D_1.findChild(QPushButton, "berandaButton_D_2")
+    _pengaturanButton_D_2 = _navbar_D_1.findChild(QPushButton, "pengaturanButton_D_2")
+    _buatAkunMhsButton_D_2 = _navbar_D_1.findChild(QPushButton, "buatAkunMhsButton_D_2")
+    # Asserting object findChild successful
+    assert _berandaButton_D_2 is not None
+    assert _pengaturanButton_D_2 is not None
+    assert _buatAkunMhsButton_D_2 is not None
 
-    # Crate and load content dashboard widget
+    # Create and load content dashboard widget
     _content_D_1 = QWidget()
     uifile = QFile(":ui/ui/dosen_content_dashboard.ui")
     uifile.open(QFile.ReadOnly)
@@ -36,11 +44,31 @@ def initDosenPage(window, auth):
     _navbar_D_1.setFixedHeight(200)
     _content_D_1.setMinimumHeight(window.frameGeometry().height() - 200)
 
+    # Set connection
+    _berandaButton_D_2.clicked.connect(lambda: berandaButtonClicked())
+    _pengaturanButton_D_2.clicked.connect(lambda: pengaturanButtonClicked())
+    _buatAkunMhsButton_D_2.clicked.connect(lambda: buatAkunMhsButtonClicked())
 
-    # # Asserting object findChild successful
-    # assert _loginBgLabel_L_2 is not None
-    # assert _usernameInput_L_2 is not None
-    # assert _emailInput_L_2 is not None
-    # assert _sendEmailButton_L_2 is not None
-    # # Set connection
-    # _sendEmailButton_L_2.clicked.connect(lambda: sendEmailButtonClicked(window))
+
+def berandaButtonClicked():
+    global _content_D_1
+    uifile = QFile(":ui/ui/dosen_content_dashboard.ui")
+    uifile.open(QFile.ReadOnly)
+    uic.loadUi(uifile, _content_D_1)
+    uifile.close()
+
+
+def pengaturanButtonClicked():
+    global _content_D_1
+    uifile = QFile(":ui/ui/dosen_content_settings.ui")
+    uifile.open(QFile.ReadOnly)
+    uic.loadUi(uifile, _content_D_1)
+    uifile.close()
+
+
+def buatAkunMhsButtonClicked():
+    global _content_D_1
+    uifile = QFile(":ui/ui/dosen_content_create_std_account.ui")
+    uifile.open(QFile.ReadOnly)
+    uic.loadUi(uifile, _content_D_1)
+    uifile.close()
