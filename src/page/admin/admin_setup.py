9e4a0d010bc7a228dfbe943d3c53b7f37d141e9b
margunwa123 <13518114@std.stei.itb.ac.id>
2020-04-13 14:15:11 +0700

    user dah bisa ditamapilin, popup belom, sisanya belom

--- src/page/admin/admin_setup.py
+++ src/page/admin/admin_setup.py
@@ -1,5 +1,5 @@
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QWidget, QScrollArea, QHBoxLayout, QVBoxLayout, QLabel, QGroupBox, QPushButton
+from PyQt5.QtWidgets import QWidget, QScrollArea, QHBoxLayout, QVBoxLayout, QLabel, QGroupBox, QPushButton, QMessageBox
 from PyQt5.QtCore import QFile, Qt
 from PyQt5 import uic
 
@@ -8,31 +8,46 @@ from util.mysql_controller import execQuery
 def setupUserContent(content):
     global _userVLayout_A_1
     _userVLayout_A_1 = content.findChild(QVBoxLayout, "userVLayout")
+    _addUser_A_1 = content.findChild(QPushButton, "addUser")
+
     # Assert that the query successful
     assert _userVLayout_A_1 is not None
 
     _userVLayout_A_1.setAlignment(Qt.AlignTop)
     _userA = QVBoxLayout()
     
+    listOfUsers = getUsers()
 
-    button = QPushButton()
-    test = QHBoxLayout()
-    test.addWidget(button)
-    setUpUserLayout(_userVLayout_A_1)
+    for i in range(listOfUsers.__len__()):
+      userBox = QGroupBox()
+      uifile = QFile(":ui/ui/admin/admin_user_box.ui")
+      uifile.open(QFile.ReadOnly)
+      uic.loadUi(uifile, userBox)
+      uifile.close()
+      userBox.setFixedHeight(50)
+      setUserBoxData(userBox, listOfUsers.__getitem__(i))
+      _userVLayout_A_1.addWidget(userBox)
+    
+    #set button onclick
+    _addUser_A_1.clicked.connect(lambda: showAddUserPopUp())
 
-    #format row : id; username; password; role; image
-    user = getUsers()
 
-def setUpUserLayout(layout:QVBoxLayout):
-  listOfUserBox = list()
-  hBox = QHBoxLayout()
-  userBox = QGroupBox()
-  userBox.setLayout(hBox)
-  uifile = QFile(":ui/ui/admin/admin_user_box.ui")
-  uifile.open(QFile.ReadOnly)
-  uic.loadUi(uifile, userBox)
-  uifile.close()
-  layout.addWidget(userBox)
+def setUserBoxData(userBox:QGroupBox, user):
+  userName = userBox.findChild(QLabel, "userName")
+  userPassword = userBox.findChild(QLabel, "userPassword")
+  userType = userBox.findChild(QLabel, "userType")
+  #format row : id; username; password; role; image
+  userName.setText(user.username)
+  userPassword.setText(user.password)
+  userType.setText(user.role)
+  userName.setAlignment(Qt.AlignCenter)
+  userPassword.setAlignment(Qt.AlignCenter)
+  userType.setAlignment(Qt.AlignCenter)
+
+def showAddUserPopUp():
+  addUserPopup = QWidget()
+  addUserPopup.setFixedHeight(500)
+  addUserPopup.setFixedWidth(500)
 
 def getUsers():
   query = """SELECT * FROM user"""
