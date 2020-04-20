46467311e2bb8ccf63834d83627bc7603f049dff
Muhammad Hasan <13518012@std.stei.itb.ac.id>
2020-04-20 18:28:29 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ from PyQt5 import QtWidgets
 
 # import etc.populate_db as populate_db
 from page.login.login_page import initLoginPage
-from util.test_login import test_login
 
 def init():
     global window, button, textEdit
